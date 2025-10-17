06763564a13089f34cc9cb4b658e84ed7d79b569
Miroslav <mirkobasista@gmail.com>
2025-10-17 10:35:45 +0200

    fix(19 users): fix redirecting route

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -87,7 +87,7 @@ class UserController extends Controller {
         $user = User::findOrFail($id);
         $user->update($validated);
         $this->roleService->assignRoles($user, $request->input('roles', []));
-        return redirect()->route('users.index')->with('success', 'User updated successfully !');
+        return redirect()->route('user.index')->with('success', 'User updated successfully !');
     }
 
     /**
@@ -98,6 +98,6 @@ class UserController extends Controller {
         $user = User::findOrFail($id);
         $user->delete();
 
-        return redirect()->route('users.index')->with('success', 'User deleted successfully!');
+        return redirect()->route('user.index')->with('success', 'User deleted successfully!');
     }
 }
