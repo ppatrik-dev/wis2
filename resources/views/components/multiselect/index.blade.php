93d4e54f70edde248d884fa7fca33575ae610670
Patrik Procházka <prochazkapatrik.dev@gmail.com>
2025-10-21 22:59:14 +0200

    fix(#8 users): fix default role selection bug

--- resources/views/components/multiselect/index.blade.php
+++ resources/views/components/multiselect/index.blade.php
@@ -2,6 +2,7 @@
     'label' => '',
     'name' => '',
     'text' => null,
+    'default' => '',
     'options' => [],
     'selected' => collect(),
     'disabled' => false
@@ -30,13 +31,16 @@
 
 <div id="dropdownMenu" class="z-10 hidden bg-white rounded-lg shadow-sm w-72 dark:bg-gray-700">
     <ul class="overflow-y-auto text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuhButton">
-        @foreach ($options as $option)
+        @foreach($options as $option)
             <li>
                 <div class="flex items-center p-1.5 rounded-sm hover:bg-gray-100 dark:hover:bg-gray-600">
+                @if($option->name === $default)
+                    <input type="hidden" name="{{ $name }}" value="{{ $default }}">
+                @endif
                 <input id="checkbox-item-{{ $option->name }}" type="checkbox" name="{{ $name }}" value="{{ $option->name }}" 
                         class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600
                          dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                        @checked($selected->contains($option->name)) @disabled($option->name === 'user')>
+                        @checked($selected->contains($option->name)) @disabled($option->name === $default)>
                 <label for="checkbox-item-{{ $option->name }}" class="w-full ms-2 text-sm font-medium text-gray-900 rounded-sm dark:text-gray-300">
                     {{ ucfirst($option->name) }}
                 </label>
