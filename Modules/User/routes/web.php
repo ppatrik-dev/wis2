7554c2dca993e2c43cd1fd64c65aa531e09c6a40
Miroslav <mirkobasista@gmail.com>
2025-10-23 09:43:25 +0200

    fix(#8 users): remove redundant user resource route and delete method

--- Modules/User/routes/web.php
+++ Modules/User/routes/web.php
@@ -6,4 +6,3 @@ use Modules\User\Http\Controllers\UserController;
 Route::resource('users', UserController::class)->names('user')->middleware('auth');
 
 Route::resource('users', UserController::class)->names('user');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
