027501ca2031ccb2f67022a89920c753d26447a5
Patrik Procházka <prochazkapatrik.dev@gmail.com>
2025-11-12 20:03:17 +0100

    fix(#23 components): adjust select and input label color

--- resources/views/components/input/index.blade.php
+++ resources/views/components/input/index.blade.php
@@ -18,14 +18,14 @@
 
 @if ($input == "input")
     <div {{ $attributes->merge([]) }}>
-        <label for="input-{{ $name }}" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+        <label for="input-{{ $name }}" class="block mb-1 text-sm font-medium text-gray-700 dark:text-gray-500">
             {{ $label }} {!! $required ? '<span class="text-blue-500">*</span>' : '' !!}
         </label>
         <input id="input-{{ $name }}" type="{{ $type }}" name="{{ $name }}" value="{{ old($name, $value) }}" placeholder="{{ $placeholder }}" class="{{ $classes }}" @required($required) @disabled($disabled)>
     </div>
 @elseif ($input == "textarea")
     <div {{ $attributes->merge([]) }}>
-        <label for="textarea-{{ $name }}" class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">{{ $label }}</label>
+        <label for="textarea-{{ $name }}" class="block mb-1 text-sm font-medium text-gray-700 dark:text-gray-500">{{ $label }}</label>
         <textarea id="textarea-{{ $name }}" rows="{{ $rows }}" name="{{ $name }}" class="{{ $classes }}" @disabled($disabled)>{{ old($name, $value) }}</textarea>
     </div>
 @endif
--- resources/views/components/select/index.blade.php
+++ resources/views/components/select/index.blade.php
@@ -8,7 +8,7 @@
 ])
 
 <div>
-    <label for="select-{{ $name }}" class="block mb-1 text-sm font-medium text-gray-500">{{ $label }} {!! $required ? '<span class="text-blue-500">*</span>' : '' !!}</label>
+    <label for="select-{{ $name }}" class="block mb-1 text-sm font-medium text-gray-700 dark:text-gray-500">{{ $label }} {!! $required ? '<span class="text-blue-500">*</span>' : '' !!}</label>
     <select id="select-{{ $name }}" name="{{ $name }}" class="group relative w-full h-9.5 p-2 flex items-center dark:text-white border border-gray-300 rounded-lg
                 bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 
                 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" @required($required) @disabled($disabled)>
