23bfcea1c503616b14170a7e7be194bd663b230f
Patrik Procházka <prochazkapatrik.dev@gmail.com>
2025-11-19 12:00:18 +0100

    feature(#39): adjust to default dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'false',
+  darkMode: 'class',
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
