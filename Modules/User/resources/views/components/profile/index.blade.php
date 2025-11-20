e0ab034f4ceda81320ba04e8ef1454747299f504
Patrik Procházka <prochazkapatrik.dev@gmail.com>
2025-11-20 21:29:13 +0100

    fix(user): adjust user profile avatar size

--- Modules/User/resources/views/components/profile/index.blade.php
+++ Modules/User/resources/views/components/profile/index.blade.php
@@ -3,11 +3,11 @@
 ])
 
 <div class="bg-white border border-gray-200 rounded-lg shadow-sm max-w dark:bg-gray-800 dark:border-gray-700">
-    <div class="flex flex-col items-center w-full h-40 py-4 rounded-t-lg bg-gray-50 dark:bg-gray-700">
+    <div class="flex flex-col items-center w-full h-full py-6 rounded-t-lg bg-gray-50 dark:bg-gray-700">
         @if ($user)
             <x-avatar letters="{{ $user->initials }}" text='4xl' width='30' height='30'></x-avatar>
         @else
-            <x-avatar width='30' height='30' top='12'></x-avatar>
+            <x-avatar width='30' height='30'></x-avatar>
         @endif
     </div>
     
