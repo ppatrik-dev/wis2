c425c7241f1d0076f18d554eca96a288497cb204
Patrik Procházka <prochazkapatrik.dev@gmail.com>
2025-11-21 11:32:31 +0100

    fix(user): adjust the user avatar icon

--- resources/views/components/avatar/index.blade.php
+++ resources/views/components/avatar/index.blade.php
@@ -2,7 +2,6 @@
     'width' => '10',
     'height' => '10',
     'text' => 'sm',
-    'top' => '3',
     'letters' => null,
 ])
 
@@ -14,10 +13,10 @@
         <span class="font-medium text-{{ $text }} text-gray-600 dark:text-gray-300">{{ $letters }}</span>
     @else
         <div class="relative bg-gray-100 rounded-full dark:bg-gray-600" style="width: {{ $width}}px; height: {{ $height}}px;">
-            <svg class="absolute text-gray-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" style="width: {{ $width  }}px; height: {{ $height}}px;">
-                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd">
-                </path>
+            <svg class="text-gray-600 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
             </svg>
+
         </div>
     @endif
 </div>
