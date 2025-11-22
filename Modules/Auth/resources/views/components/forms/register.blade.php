a22180965780890ca4ad96a7297c269a533a402a
Patrik Procházka <prochazkapatrik.dev@gmail.com>
2025-11-22 12:32:48 +0100

    fix(register): adjust the birth date input

--- Modules/Auth/resources/views/components/forms/register.blade.php
+++ Modules/Auth/resources/views/components/forms/register.blade.php
@@ -32,7 +32,7 @@
                       <input type="password" name="password_confirmation" id="password_confirmation" placeholder="*********" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                   </div>
                      <div>
-                      <label for="country" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
+                      <label for="country" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country:</label>
                       <input type="country" name="country" id="country" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="America">
                   </div>
                     <div>
@@ -44,7 +44,7 @@
                         <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                         </svg>
                     </div>
-                    <input datepicker id="birth_date" name="birth_date" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="01.01.1970">
+                    <input id="birth_date" name="birth_date" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="01.01.1970">
                     </div>
                     </div>
                   <div>
