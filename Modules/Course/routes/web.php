e41f79d6569e83416a60c97bd08f7f3b8a388fc1
Miroslav <mirkobasista@gmail.com>
2025-11-23 07:11:29 +0100

    feature(routes): add authentication middleware to course and term routes

--- Modules/Course/routes/web.php
+++ Modules/Course/routes/web.php
@@ -9,10 +9,10 @@ use Modules\Course\App\Http\Controllers\MyCourseController;
 
 Route::resource('courses', CourseController::class)->names('course');
 Route::get('my-courses', [MyCourseController::class, 'index'])->name('course.my-courses')->middleware('auth');
-Route::patch('courses/{course}/approve', [CourseController::class, 'approve'])->name('course.approve');
+Route::patch('courses/{course}/approve', [CourseController::class, 'approve'])->name('course.approve')->middleware('auth');
 
 Route::get('courses/{course}/students/lookup', [CourseStudentController::class, 'lookupPublic'])
-    ->name('course.student.lookup');
+    ->name('course.student.lookup')->middleware('auth');
 
 // Bulk register/unregister for the currently authenticated user (from course listing)
 Route::post('courses/students/register-multiple', [CourseStudentController::class, 'registerMultiple'])
@@ -25,22 +25,22 @@ Route::post('courses/students/update-registrations', [CourseStudentController::c
 
 Route::prefix('courses/{course}')->group(function () {
     // Course lecturers
-    Route::resource('lecturers', CourseLecturerController::class)->names('course.lecturer');
+    Route::resource('lecturers', CourseLecturerController::class)->names('course.lecturer')->middleware('auth');
 
     // Course news
-    Route::resource('news', CourseNewsController::class)->names('course.news');
-    Route::get('news/search', [CourseNewsController::class, 'search'])->name('course.news.search');
+    Route::resource('news', CourseNewsController::class)->names('course.news')->middleware('auth');
+    Route::get('news/search', [CourseNewsController::class, 'search'])->name('course.news.search')->middleware('auth');
 
     // Course students
     Route::resource('students', CourseStudentController::class)->names('course.student');
-    Route::get('students/approved', [CourseStudentController::class, 'approved'])->name('course.student.approved');
-    Route::get('students/pending', [CourseStudentController::class, 'pending'])->name('course.student.pending');
-    Route::get('students/scores', [CourseStudentController::class, 'scores'])->name('course.student.scores');
-    Route::post('students/bulk-approve', [CourseStudentController::class, 'bulkApprove'])->name('course.student.bulk-approve');
-    Route::post('students/bulk-reject', [CourseStudentController::class, 'bulkReject'])->name('course.student.bulk-reject');
-    Route::patch('students/{student}/approve', [CourseStudentController::class, 'approve'])->name('course.student.approve');
-    Route::patch('students/{student}/reject', [CourseStudentController::class, 'reject'])->name('course.student.reject');
-    Route::patch('students/{student}/score', [CourseStudentController::class, 'updateScore'])->name('course.student.update-score');
+    Route::get('students/approved', [CourseStudentController::class, 'approved'])->name('course.student.approved')->middleware('auth');
+    Route::get('students/pending', [CourseStudentController::class, 'pending'])->name('course.student.pending')->middleware('auth');
+    Route::get('students/scores', [CourseStudentController::class, 'scores'])->name('course.student.scores')->middleware('auth');
+    Route::post('students/bulk-approve', [CourseStudentController::class, 'bulkApprove'])->name('course.student.bulk-approve')->middleware('auth');
+    Route::post('students/bulk-reject', [CourseStudentController::class, 'bulkReject'])->name('course.student.bulk-reject')->middleware('auth');
+    Route::patch('students/{student}/approve', [CourseStudentController::class, 'approve'])->name('course.student.approve')->middleware('auth');
+    Route::patch('students/{student}/reject', [CourseStudentController::class, 'reject'])->name('course.student.reject')->middleware('auth');
+    Route::patch('students/{student}/score', [CourseStudentController::class, 'updateScore'])->name('course.student.update-score')->middleware('auth');
     // Register current authenticated user for this course (checkbox + submit flow)
     Route::post('students/register', [CourseStudentController::class, 'registerCurrentUser'])->name('course.student.register')->middleware('auth');
     Route::post('students/unregister', [CourseStudentController::class, 'unregisterCurrentUser'])->name('course.student.unregister')->middleware('auth');
--- Modules/Term/routes/web.php
+++ Modules/Term/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file web.php
  * @author Patrik Procházka (xprochp00@vutbr.cz)
@@ -14,25 +15,27 @@ use Modules\Term\Http\Controllers\TimeTableController;
 use Modules\Term\Http\Controllers\RoomController;
 use Modules\Term\Http\Controllers\TermStudentController;
 
-Route::resource('terms', TermController::class)->names('term');
 
-Route::prefix('terms/{term}')->group(function () {
-    Route::post('students/register', [TermStudentController::class, 'register'])
-        ->name('term.student.register');
+Route::middleware('auth')->group(function () {
+    Route::resource('terms', TermController::class)->names('term');
 
-    Route::post('students/unregister', [TermStudentController::class, 'unregister'])
-        ->name('term.student.unregister');
-        
-    Route::resource('students', TermStudentController::class)
-        ->parameters(['students' => 'student'])
-        ->names('term.student');
+    Route::prefix('terms/{term}')->group(function () {
+        Route::post('students/register', [TermStudentController::class, 'register'])
+            ->name('term.student.register');
 
+        Route::post('students/unregister', [TermStudentController::class, 'unregister'])
+            ->name('term.student.unregister');
+
+        Route::resource('students', TermStudentController::class)
+            ->parameters(['students' => 'student'])
+            ->names('term.student');
+    });
+
+    Route::post('/terms/{term}/register', [TermController::class, 'register'])
+        ->name('term.register');
+
+    Route::resource('rooms', RoomController::class)->names('room');
+
+    Route::get('timetable', [TimetableController::class, 'index'])
+        ->name('timetable.index');
 });
-
-Route::post('/terms/{term}/register', [TermController::class, 'register'])
-    ->name('term.register');
-
-Route::resource('rooms', RoomController::class)->names('room');
-
-Route::get('timetable', [TimetableController::class, 'index'])
-    ->name('timetable.index');
